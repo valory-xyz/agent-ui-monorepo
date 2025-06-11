965d0538be86cd1874244ad934585ba0c53d16d4
mohandast52 <mohandast52@gmail.com>
2025-06-11 18:42:22 +0530

    feat: add modius-ui-example app

---
+++ modius-ui-example/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'modius-ui-example',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/modius-ui-example',
+};
