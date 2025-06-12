c5a14cbd78b117da9d5a91ee9289412d0276ded0
mohandast52 <mohandast52@gmail.com>
2025-06-12 19:08:53 +0530

    feat(Navbar): add userName prop to Navbar component and update related tests; implement generateAgentName utility function

--- libs/ui-navbar/src/lib/ui-navbar.tsx
+++ libs/ui-navbar/src/lib/ui-navbar.tsx
@@ -2,6 +2,7 @@ import { Flex, Typography } from 'antd';
 import { CSSProperties, useMemo } from 'react';
 import modiusLogo from '../assets/agent-modius-logo.png';
 import optimusLogo from '../assets/agent-optimus-logo.png';
+import { generateAgentName } from '../utils/generateAgentName';
 
 const { Title, Text } = Typography;
 
@@ -30,40 +31,30 @@ const NavContent = ({ imgUrl, title, description }: NavContentProps) => {
   );
 };
 
-type NavbarProps = { agentType: string }; // TODO: convert to agentType
+type NavbarProps = { agentType: string; userName: string }; // TODO: convert to agentType
 
-export function Navbar({ agentType }: NavbarProps) {
-  const imgUrls = useMemo(() => {
-    if (agentType === 'modius') {
-      return { agent: modiusLogo, user: modiusLogo }; // TODO: user image
+export function Navbar({ agentType, userName }: NavbarProps) {
+  const { imgUrls, agentDetails, userDetails } = useMemo(() => {
+    switch (agentType) {
+      case 'modius':
+        return {
+          imgUrls: { agent: modiusLogo, user: modiusLogo }, // TODO: user image
+          agentDetails: { agent: 'Modius', desc: 'Agent Economy' },
+          userDetails: { desc: 'Modius agent' },
+        };
+      case 'optimus':
+        return {
+          imgUrls: { agent: optimusLogo, user: optimusLogo }, // TODO: user image
+          agentDetails: { agent: 'Optimus', desc: 'Agent Economy' },
+          userDetails: { desc: 'Optimus agent' },
+        };
+      default:
+        throw new Error('Unsupported agent type');
     }
-
-    if (agentType === 'optimus') {
-      return { agent: optimusLogo, user: optimusLogo }; // TODO: user image
-    }
-
-    throw new Error('Unsupported agent type');
   }, [agentType]);
 
-  const agentDetails = useMemo(() => {
-    if (agentType === 'modius') {
-      return { agent: 'Modius', desc: 'Agent Economy' };
-    } else if (agentType === 'optimus') {
-      return { agent: 'Optimus', desc: 'Agent Economy' };
-    }
-
-    throw new Error('Unsupported agent type');
-  }, [agentType]);
-
-  const userDetails = useMemo(() => {
-    if (agentType === 'modius') {
-      return { user: 'modius-user', desc: 'Modius agent' };
-    } else if (agentType === 'optimus') {
-      return { user: 'optimus-user', desc: 'Optimus agent' };
-    }
-
-    throw new Error('Unsupported agent type');
-  }, [agentType]);
+  const userDisplayName = generateAgentName(userName);
+  // TODO: icon to be added for user
 
   return (
     <Flex justify="space-between" align="middle" style={style}>
@@ -72,7 +63,7 @@ export function Navbar({ agentType }: NavbarProps) {
         title={agentDetails.agent}
         description={agentDetails.desc}
       />
-      <NavContent imgUrl={imgUrls.user} title={userDetails.user} description={userDetails.desc} />
+      <NavContent imgUrl={imgUrls.user} title={userDisplayName} description={userDetails.desc} />
     </Flex>
   );
 }
