274af7a5e1139ef3fae7378b451eb61dc7e8e93c
Mohan <mohandast52@gmail.com>
2025-06-17 15:27:29 +0530

    fix: update cache and output directories in vite config for ui-navbar (#10)

--- libs/ui-navbar/vite.config.ts
+++ libs/ui-navbar/vite.config.ts
@@ -8,7 +8,7 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig(() => ({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/ui-navbar',
+  cacheDir: '../../node_modules/.vite/libs/ui-navbar',
   plugins: [
     react(),
     nxViteTsPaths(),
@@ -25,7 +25,7 @@ export default defineConfig(() => ({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../dist/ui-navbar',
+    outDir: '../../dist/libs/ui-navbar',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
