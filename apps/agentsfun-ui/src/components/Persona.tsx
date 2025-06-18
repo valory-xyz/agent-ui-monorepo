cac9e65bbc6a33c8fc36629945f10ed7aa40ff78
mohandast52 <mohandast52@gmail.com>
2025-06-19 04:45:28 +0530

    feat: add loading skeleton for persona description and refactor layout

--- apps/agentsfun-ui/src/components/Persona.tsx
+++ apps/agentsfun-ui/src/components/Persona.tsx
@@ -1,10 +1,18 @@
-import { Button, Flex, Typography } from 'antd';
+import { Button, Flex, Skeleton, Typography } from 'antd';
 import { Card } from './ui/Card';
 import { useAgentDetails } from '../hooks/useAgentDetails';
 import { useState } from 'react';
 import { DownOutlined, UpOutlined } from '@ant-design/icons';
 
 const { Title, Text, Paragraph } = Typography;
+
+const DescriptionLoading = () => (
+  <Flex vertical align="flex-start" gap={8}>
+    <Skeleton.Input active block size="small" />
+    <Skeleton.Input active block size="small" />
+    <Skeleton.Input active size="small" style={{ width: 200 }} />
+  </Flex>
+);
 
 const Description = ({ desc }: { desc: string }) => {
   const [ellipsis, setEllipsis] = useState(true);
@@ -29,36 +37,53 @@ const Description = ({ desc }: { desc: string }) => {
   );
 };
 
+// TODO: ask design team about error handling states.
+/**
+ * Displays the agent's persona information.
+ */
 export const Persona = () => {
   const { data: agentDetails } = useAgentDetails();
 
   return (
-    <Card>
-      <Flex vertical gap={24}>
-        <Flex vertical align="flex-start">
-          <Title level={3} className="m-0">
-            {agentDetails?.name}
-          </Title>
-          <Button
-            onClick={() => window.open(`https://x.com/${agentDetails?.username}`, '_blank')}
-            type="link"
-            className="p-0"
-          >
-            {`@${agentDetails?.username}`}
-          </Button>
-        </Flex>
+    <>
+      <Card>
+        <Flex vertical gap={24}>
+          <Flex vertical align="flex-start">
+            <Title level={3} className="m-0">
+              {agentDetails?.name}
+            </Title>
+            <Button
+              onClick={() => window.open(`https://x.com/${agentDetails?.username}`, '_blank')}
+              type="link"
+              className="p-0"
+            >
+              {`@${agentDetails?.username}`}
+            </Button>
+          </Flex>
 
-        <Flex vertical gap={8}>
-          <Text type="secondary">Persona</Text>
-          <Description desc={agentDetails?.personaDescription || ''} />
+          <Flex vertical gap={8}>
+            <Text type="secondary">Persona</Text>
+            <Description desc={agentDetails?.personaDescription || ''} />
+          </Flex>
         </Flex>
-      </Flex>
-    </Card>
+      </Card>
+
+      {/* loader */}
+      <Card>
+        <Flex vertical gap={24} style={{ width: '100%' }}>
+          <Flex vertical align="flex-start">
+            <Title level={3} className="m-0">
+              <Skeleton.Input active />
+            </Title>
+            <Skeleton.Input active size="small" style={{ marginTop: 12 }} />
+          </Flex>
+
+          <Flex vertical gap={8} style={{ width: '100%' }}>
+            <Text type="secondary">Persona</Text>
+            <DescriptionLoading />
+          </Flex>
+        </Flex>
+      </Card>
+    </>
   );
 };
-
-/**
- * TODO:
- * - add loader
- * - add error state
- */
