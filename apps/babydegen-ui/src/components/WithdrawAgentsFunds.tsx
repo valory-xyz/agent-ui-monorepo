92b78251412a8e20d8c90bca94f07365e53a633b
mohandast52 <mohandast52@gmail.com>
2025-07-08 01:37:47 +0530

    feat: implement withdrawal logic in WithdrawAgentsFunds component

--- apps/babydegen-ui/src/components/WithdrawAgentsFunds.tsx
+++ apps/babydegen-ui/src/components/WithdrawAgentsFunds.tsx
@@ -1,20 +1,39 @@
 import { Button, Card, Flex, Typography } from 'antd';
 
 import { CardTitle } from '../ui/CardTitle';
+import { useState } from 'react';
+import { COLOR } from '../constants/colors';
 
 const { Text } = Typography;
 
-export const WithdrawAgentsFunds = () => (
-  <Card className="card-border card-gradient">
-    <Flex vertical justify="space-between" align="flex-start" gap={16}>
-      <CardTitle text="Withdraw agents funds" />
-      <Text type="secondary">
-        Transfer all assets currently managed by your agent to your target address, converted to
-        USDC. This action is recommended before initiating the withdrawal process on Pearl.
-      </Text>
-      <Button type="primary" style={{ color: 'black' }}>
-        Withdraw
-      </Button>
-    </Flex>
-  </Card>
-);
+export const WithdrawInvestedFunds = () => <>TODO</>;
+
+export const WithdrawAgentsFunds = () => {
+  const [canInitiateWithdrawal, setCanInitiateWithdrawal] = useState(false);
+
+  return (
+    <Card className="card-gradient">
+      <Flex vertical justify="space-between" align="flex-start" gap={16}>
+        <CardTitle
+          text={canInitiateWithdrawal ? 'Withdraw agents funds' : 'Withdraw invested funds'}
+        />
+        <Text type="secondary">
+          Transfer all assets currently managed by your agent to your target address, converted to
+          USDC. This action is recommended before initiating the withdrawal process on Pearl.
+        </Text>
+
+        {canInitiateWithdrawal ? (
+          <WithdrawInvestedFunds />
+        ) : (
+          <Button
+            onClick={() => setCanInitiateWithdrawal(true)}
+            type="primary"
+            style={{ color: COLOR.black }}
+          >
+            Withdraw
+          </Button>
+        )}
+      </Flex>
+    </Card>
+  );
+};
