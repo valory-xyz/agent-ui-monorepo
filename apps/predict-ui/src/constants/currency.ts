2a86d7a29aecbe4236926d7b4f1adf1e60d3aff4
Jenslee Dsouza <33950242+LOCKhart07@users.noreply.github.com>
2026-02-24 20:15:38 +0530

    fix: add more currency to symbol map (#79)
    * fix: add more currency to symbol map
    
    * fix: add USDC.e to symbol map

--- apps/predict-ui/src/constants/currency.ts
+++ apps/predict-ui/src/constants/currency.ts
@@ -1,5 +1,8 @@
 export const CURRENCY = {
   USD: { symbol: '$', name: 'US Dollar' },
+  USDC: { symbol: '$', name: 'US Dollar' },
+  USDT: { symbol: '$', name: 'US Dollar' },
+  'USDC.e': { symbol: '$', name: 'US Dollar' },
 } as const;
 
 export type CurrencyCode = keyof typeof CURRENCY;
